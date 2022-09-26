67196953774b3dd7919881e42acad4c32364acb6
AlexanderDmitriev <alex9110077@gmail.com>
2022-09-26 22:21:43 +0200

    Squashed commit of the following:
    commit 0705d5bc8bff64c1c2fd3bde7d873f54c5c54541
    Author: AlexanderDmitriev <alex9110077@gmail.com>
    Date:   Mon Sep 26 22:20:20 2022 +0200
    
        Update App.jsx
    
    commit 51e65c5cc8bac68f7b994ee94f2eda1d28029a84
    Merge: a280579 1a15137
    Author: AlexanderDmitriev <alex9110077@gmail.com>
    Date:   Mon Sep 26 20:18:42 2022 +0200
    
        Merge pull request #11 from AlexanderDmitriev/dist
    
        Dist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,9 @@ import NotFoundPage from '../pages/NotFoundPage';
 /* import Container from './Container'; */
 
 const FirstPage = lazy(() => import('../components/FirstPage/FirstPage'));
-const Library = lazy(() => import('./library/Library/Library'));
 const Register = lazy(() => import('../pages/Register'));
 const Login = lazy(() => import('../pages/Login'));
+const Library = lazy(() => import('../components/library/library'));
 
 export const App = () => {
   return (
@@ -19,7 +19,7 @@ export const App = () => {
             <Route path="/" element={<FirstPage />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
-            <Route path="library" element={<Library />} />
+            <Route path="/library" element={<Library />} />
             {/* <Training/>*/}
             {/* <Statistics/>*/}
             <Route path="*" element={<NotFoundPage />} />
