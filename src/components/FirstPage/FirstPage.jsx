c43f15915a1b3dd8e374ce704c4d9d646c21ec22
AlexanderDmitriev <alex9110077@gmail.com>
2022-09-29 00:27:45 +0200

    fix first page, dublicate library modal

--- src/components/FirstPage/FirstPage.jsx
+++ src/components/FirstPage/FirstPage.jsx
@@ -8,8 +8,12 @@ import {
   LoginButton,
   RegisterButton,
   MainListItem,
+  Icon,
+  Text,
+  IconWrapper,
 } from './FirstPage.styled';
 import { NavLink } from 'react-router-dom';
+import sprite from '../../images/icons.svg';
 
 export default function FirstPage() {
   return (
@@ -17,33 +21,75 @@ export default function FirstPage() {
       <Title>Books Reading</Title>
       <List>
         <MainListItem>
-          <Subtitle>Will help you to</Subtitle>
+          <Subtitle>Допоможе вам</Subtitle>
           <List>
-            <ListItem>Create your goal faster and proceed to read</ListItem>
-            <ListItem>Divide process proportionally for each day</ListItem>
-            <ListItem>Track your success</ListItem>
+            <ListItem>
+              <IconWrapper>
+                <Icon width="4" height="8">
+                  <use href={sprite + '#icon-firstpagevector'}></use>
+                </Icon>
+              </IconWrapper>
+              <Text>Швидше сформулювати свою ціль і розпочати читати</Text>
+            </ListItem>
+            <ListItem>
+              <IconWrapper>
+                <Icon width="4" height="8">
+                  <use href={sprite + '#icon-firstpagevector'}></use>
+                </Icon>
+              </IconWrapper>
+              <Text>Пропорційно розподілити навантаження на кожний день</Text>
+            </ListItem>
+            <ListItem>
+              <IconWrapper>
+                <Icon width="4" height="8">
+                  <use href={sprite + '#icon-firstpagevector'}></use>
+                </Icon>
+              </IconWrapper>
+              <Text>Відстежувати особистий успіх</Text>
+            </ListItem>
           </List>
         </MainListItem>
         <MainListItem>
-          <Subtitle>You may also</Subtitle>
+          <Subtitle>Також ви зможете</Subtitle>
           <List>
-            <ListItem>Pose your own independent point of view</ListItem>
             <ListItem>
-              Improve your professional skills according to new knowledge
+              <IconWrapper>
+                <Icon width="4" height="8">
+                  <use href={sprite + '#icon-firstpagevector'}></use>
+                </Icon>
+              </IconWrapper>
+              <Text>Формувати особисту думку незалежну від інших</Text>
             </ListItem>
-            <ListItem>Become an interesting interlocutor</ListItem>
+            <ListItem>
+              <IconWrapper>
+                <Icon width="4" height="8">
+                  <use href={sprite + '#icon-firstpagevector'}></use>
+                </Icon>
+              </IconWrapper>
+              <Text>
+                Підвищити свої професійні якості опираючись на нові знання
+              </Text>
+            </ListItem>
+            <ListItem>
+              <IconWrapper>
+                <Icon width="4" height="8">
+                  <use href={sprite + '#icon-firstpagevector'}></use>
+                </Icon>
+              </IconWrapper>
+              <Text>Стати цікавим співрозмовником</Text>
+            </ListItem>
           </List>
         </MainListItem>
       </List>
       <ButtonList>
         <li>
           <NavLink to="/login" exact="true">
-            <LoginButton>Log in</LoginButton>
+            <LoginButton>Увійти</LoginButton>
           </NavLink>
         </li>
         <li>
           <NavLink to="/register" exact="true">
-            <RegisterButton>Register</RegisterButton>
+            <RegisterButton>Реєстрація</RegisterButton>
           </NavLink>
         </li>
       </ButtonList>
