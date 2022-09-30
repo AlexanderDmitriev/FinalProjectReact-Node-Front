8fcac07a7f336852c6acc6b2e3510b99bed34602
AlexanderDmitriev <alex9110077@gmail.com>
2022-09-30 11:59:51 +0200

    Update auth-operation.js

--- src/redux/authAPI/auth-operation.js
+++ src/redux/authAPI/auth-operation.js
@@ -16,7 +16,7 @@ const register = createAsyncThunk(
   '/auth/register',
   async (credentials, { rejectWithValue }) => {
     try {
-      const { data } = await axios.post('/users/signup', credentials);
+      const { data } = await axios.post('/api/users/signup', credentials);
       token.set(data.token);
       return data;
     } catch (error) {
