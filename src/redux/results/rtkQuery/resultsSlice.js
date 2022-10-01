d5ff1da244cc4e367403f1ea2610bc38a991aa2c
Viktoriia Denysenko <denysenko.v.t@gmail.com>
2022-10-01 20:32:07 +0300

    fix RTK Query, add axiosBaseQuery
    fix RTK Query, add axiosBaseQuery

--- src/redux/results/rtkQuery/resultsSlice.js
+++ src/redux/results/rtkQuery/resultsSlice.js
@@ -1,23 +1,43 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
+import axios from 'axios';
+
+const axiosBaseQuery =
+  ({ baseUrl } = { baseUrl: '' }) =>
+  async ({ url, method, data, params }) => {
+    try {
+      const result = await axios({ url: baseUrl + url, method, data, params });
+      return { data: result.data };
+    } catch (axiosError) {
+      let err = axiosError;
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      };
+    }
+  };
 
 export const resultsApi = createApi({
   reducerPath: 'resultsApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://book-reader-43-back.herokuapp.com/',
+  baseQuery: axiosBaseQuery({
+    baseUrl: 'https://book-reader-43-back.herokuapp.com/api/',
   }),
   tagTypes: ['Result'],
   endpoints: builder => ({
     fetchResults: builder.query({
-      query: () => 'training/stats',
+      query: () => ({
+        url: 'training/stats',
+      }),
       providesTags: ['Result'],
     }),
     createResult: builder.mutation({
       query: result => ({
-        url: '/training/stats',
+        url: 'training/stats',
         method: 'POST',
-        body: {
+        data: {
           date: result.date,
-          time: result.time,
+          // time: result.time,
           pages: result.pages,
         },
       }),
