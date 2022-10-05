87936b47aeffe68e01def4934e8fc600d78f7cdc
AlexanderDmitriev <alex9110077@gmail.com>
2022-10-05 03:47:50 +0200

    Update Training.jsx

--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -5,12 +5,14 @@ import Results from 'components/Results/Results';
 import AddTraining from 'components/Training/AddTraining/AddTraining';
 import Chart from 'components/Chart/Chart';
 import { useGetBooksQuery } from 'redux/booksApi/booksSlice';
+import Timers from 'components/Timers/Timers';
 
 export default function Training() {
   const { data, /* error, */ isLoading } = useGetBooksQuery();
 
   return (
     <TrainingPage>
+      <Timers/>
       <AddTraining />
       {!isLoading && <Chart books={data} />}
       <Results />
