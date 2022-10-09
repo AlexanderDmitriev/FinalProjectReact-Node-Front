4068f83a08b15af6299167b48ea93820db8dcf94
AlexanderDmitriev <alex9110077@gmail.com>
2022-10-09 19:35:35 +0200

    Update Training.jsx

--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -7,13 +7,25 @@ import AddTraining from 'components/Training/AddTraining/AddTraining';
 import Chart from 'components/Chart/Chart';
 import { useGetBooksQuery } from 'redux/booksApi/booksSlice';
 import Timers from 'components/Timers/Timers';
+import Modal from 'components/Modal/Modal';
+import {
+  Wrapper,
+  SectionM,
+  TextBox,
+  Text,
+  ButtonBox,
+  ButtonM,
+  IconLike,
+} from '../components/Results/Modal.styled';
+import sprite from '../images/icons.svg';
+import { useNavigate /* , useLocation */ } from 'react-router-dom';
 
 export default function Training() {
   const { data, isLoading } = useGetBooksQuery();
   // const [finishDate, setFinishDate] = useState(Date.now());
-  const [start, setStart] = useState('');
-  const [finish, setFinish] = useState('');
-  const [books, setBooks] = useState([]);
+  const [/* start */, setStart] = useState('');
+  const [/* finish */, setFinish] = useState('');
+  const [/* books */, setBooks] = useState([]);
 
   const useQueryStateResult = resultsApi.endpoints.fetchResults.useQueryState();
   // const handleFinishDate = date => {
@@ -29,9 +41,22 @@ export default function Training() {
   const setDataBooks = books => {
     setBooks(books);
   };
-  console.log('start', start);
-  console.log('finish', finish);
-  console.log('books', books);
+
+
+  /********************************************** */
+  const [isModalOpen, setModalOpen] = useState(false);
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+  const navigate = useNavigate();
+  const handleExit = () => {
+    setModalOpen(false);
+    navigate('/library', { replace: true });
+  };
+
+  const queryStateResult = resultsApi.endpoints.fetchResults.useQueryState();
+  const isDone =queryStateResult?.data?.status === 'done';
+  //**************************************** */
   return (
     <TrainingPage>
       {useQueryStateResult.data &&
@@ -44,6 +69,26 @@ export default function Training() {
       {!isLoading && <Chart books={data} />}
       {useQueryStateResult.data &&
         useQueryStateResult.data.status === 'in progress' && <Results />}
+
+      <Modal isModalOpen={isModalOpen} handleCloseModal={handleCloseModal}>
+        <Wrapper>
+          {isDone && (
+            <SectionM>
+              <IconLike width="50" height="45">
+                <use href={sprite + '#icon-vector'}></use>
+              </IconLike>
+              <TextBox>
+                <Text>Вітаю! Тренування успішно закінчено!</Text>
+              </TextBox>
+              <ButtonBox>
+                <ButtonM type="button" onClick={handleExit}>
+                  Готово
+                </ButtonM>
+              </ButtonBox>
+            </SectionM>
+          )}
+        </Wrapper>
+      </Modal>
     </TrainingPage>
   );
 }
