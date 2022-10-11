d83a1e46328bc0adfcd27bbd2e7018886b7cd072
AlexanderDmitriev <alex9110077@gmail.com>
2022-10-11 22:35:53 +0200

    2

--- src/helpers/Container.jsx
+++ src/helpers/Container.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Container = styled.div`
   box-sizing: content-box;
   margin: 0 auto;
-  width: 320px;
+  width: 100%;
 
   @media screen and (min-width: 768px) {
     width: 768px;
